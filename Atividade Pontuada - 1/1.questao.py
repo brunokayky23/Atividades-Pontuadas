c5821ac5e11a6495409465338ab0aecebb3aabe9
Bruno Kayky <85810462+brunokayky23@users.noreply.github.com>
2025-03-13 23:05:00 +0000

    Atividade Pontuada

---
+++ Atividade Pontuada - 1/1.questao.py
@@ -0,0 +1,6 @@
+import os
+os.system("clear")
+
+
+# Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C, caso contrário, imprima que a A + B é maior que C. 
+
