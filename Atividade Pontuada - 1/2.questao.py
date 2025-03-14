c7f4c15467e545f448d38d24f06da0740f7566d6
Bruno Kayky <85810462+brunokayky23@users.noreply.github.com>
2025-03-14 12:55:59 +0000

    Salvando Atividade

--- Atividade Pontuada - 1/2.questao.py
+++ Atividade Pontuada - 1/2.questao.py
@@ -7,3 +7,38 @@ os.system("clear")
 # Por fim, mostre os dados do usuário.
 
 
+nome = input("Digite seu nome: ")
+
+sexo = input("""
+M - Masculino
+F - Feminino             
+Digite a letra correspondente ao seu sexo:
+""").upper() 
+while sexo not in ["M", "F"]:
+    print("Resposta está errada! Digite 'M' para masculino ou 'F' para feminino.")
+    sexo = input("Digite seu sexo corretamente (M para Masculino, F para Feminino): ").upper()
+
+estado_civil = input("""
+Casado(a)
+Solteiro (a)
+Digite seu estado civil: 
+""").upper()
+
+
+
+if sexo == "F" and estado_civil == "CASADA":
+    tempo_casada = int(input("Quantos anos você está casada? "))
+    print("")
+    print(f"Tempo de casada: {tempo_casada} anos")
+else:
+    print("")
+
+
+print(f"Nome: {nome}")
+
+if sexo == "F":
+    print("Sexo: Feminino")
+else:
+    print("Sexo: Masculino")
+
+print(f"Estado Civil: {estado_civil}")
