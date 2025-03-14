d5634bc24b6efd32c2f722c80b3b7ac4c56f367c
Bruno Kayky <85810462+brunokayky23@users.noreply.github.com>
2025-03-14 01:45:43 +0000

    Salvando Atividade

--- Atividade Pontuada - 1/1.questao.py
+++ Atividade Pontuada - 1/1.questao.py
@@ -2,5 +2,21 @@ import os
 os.system("clear")
 
 
-# Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C, caso contrário, imprima que a A + B é maior que C. 
+# Faça um algoritmo que leia os valores A, B, C 
+# e imprima na tela se a soma de A + B é menor que C, 
+# caso contrário, imprima que a A + B é maior que C. 
 
+valor_a = float(input("Digite o primeiro valor: "))
+valor_b = float(input("Digite o segundo valor: "))
+valor_c = float(input("Digite o terceiro valor: "))
+
+soma = valor_a + valor_b
+
+
+if soma < valor_c:
+    print(f"O valor {soma} é menor que {valor_c}")
+elif soma > valor_c: 
+    print(f"O valor {soma} é maior que {valor_c}")
+else:
+    print(f"O valor {soma} é igual a {valor_c}")
+print("")
---
+++ Atividade Pontuada - 1/2.questao.py
@@ -0,0 +1,9 @@
+import os
+os.system("clear")
+
+
+# Faça um algoritmo que leia o nome, o sexo e o estado civil de uma pessoa. 
+# Caso sexo seja “F” e estado civil seja “CASADA”, solicitar o tempo de casada (anos).
+# Por fim, mostre os dados do usuário.
+
+
